4e9a5cca289aee396d61971ffdf3688b4420a23d
Azamat <azamat.ahmetshin93@gmail.com>
2024-01-29 20:47:22 +0300

    Todo points deleted

--- app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
+++ app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
@@ -33,8 +33,6 @@ import com.example.dogglers.data.DataSource
 class DogCardAdapter(
     private val context: Context?, private val layout: Int
 ) : RecyclerView.Adapter<DogCardAdapter.DogCardViewHolder>() {
-
-    // TODO: Initialize the data using the List found in data/DataSource
     val dataSet = DataSource.dogs
 
     /**
@@ -56,14 +54,6 @@ class DogCardAdapter(
                 .inflate(R.layout.vertical_horizontal_list_item, parent, false)
         }
 
-        // TODO: Use a conditional to determine the layout type and set it accordingly.
-        //  if the layout variable is Layout.GRID the grid list item should be used. Otherwise the
-        //  the vertical/horizontal list item should be used.
-
-        // TODO Inflate the layout
-
-        // TODO: Null should not be passed into the view holder. This should be updated to reflect
-        //  the inflated layout.
         return DogCardViewHolder(adapterLayout)
     }
 
@@ -76,14 +66,5 @@ class DogCardAdapter(
         holder.ageTextView.text = context?.resources?.getString(R.string.dog_age, item.age)
         holder.hobbiesTextView.text =
             context?.resources?.getString(R.string.dog_hobbies, item.hobbies)
-        // TODO: Get the data at the current position
-        // TODO: Set the image resource for the current dog
-        // TODO: Set the text for the current dog's name
-        // TODO: Set the text for the current dog's age
-        val resources = context?.resources
-        // TODO: Set the text for the current dog's hobbies by passing the hobbies to the
-        //  R.string.dog_hobbies string constant.
-        //  Passing an argument to the string resource looks like:
-        //  resources?.getString(R.string.dog_hobbies, dog.hobbies)
     }
 }
